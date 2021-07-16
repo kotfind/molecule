7fe2a95710bfb785507144e4070995c9478dc0f1
kotfind <kotfind@yandex.ru>
2021-07-16 09:31:40 +0300

    Temporary axis

--- src/Engine/start.cpp
+++ src/Engine/start.cpp
@@ -16,8 +16,19 @@ void Engine::start() {
 
         SDL_GetRendererOutputSize(ren_, &screen_size_.first,
                 &screen_size_.second);
+
+        Atom zero ("ZERO", -1, {0., 0., 0.});
+        Atom xaxis("XAXI", -1, {1., 0., 0.});
+        Atom yaxis("YAXI", -1, {0., 1., 0.});
+        Atom zaxis("ZAXI", -1, {0., 0., 1.});
+        Bond xbond(&zero, &xaxis);
+        Bond ybond(&zero, &yaxis);
+        Bond zbond(&zero, &zaxis);
         
         m->draw(*this);
+        xbond.draw(*this);
+        ybond.draw(*this);
+        zbond.draw(*this);
 
         SDL_RenderPresent(ren_);
     }
