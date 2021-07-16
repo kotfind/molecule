95e4f6e0cba47112b36792929ba7de44ae49d804
kotfind <kotfind@yandex.ru>
2021-07-16 11:03:23 +0300

    Fix grafix

--- src/Atom/visible.cpp
+++ src/Atom/visible.cpp
@@ -3,5 +3,5 @@
 #include "../Engine/Engine.h"
 
 bool Atom::visible(const Engine& engine) const {
-    return pos_.z - engine.camera_pos_.z < 0;
+    return ((vec)(engine.matrix_ * pos_)).z - engine.camera_pos_.z > 0;
 }
