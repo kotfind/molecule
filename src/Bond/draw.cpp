5d4927071ff446c0be43b9d5e22ca6425577e009
kotfind <kotfind@yandex.ru>
2021-07-20 10:32:32 +0300

    Colour bonds

--- src/Bond/draw.cpp
+++ src/Bond/draw.cpp
@@ -8,7 +8,19 @@ void Bond::draw(const Engine& engine) const {
             !val_.second->visible(engine)) return;
     const auto a = val_.first->project(engine);
     const auto b = val_.second->project(engine);
-    SDL_SetRenderDrawColor(engine.ren_, 0xff, 0xff, 0xff, 0xff);
+    if ((val_.first == engine.selected_[0] &&
+         val_.second == engine.selected_[1]) ||
+        (val_.first == engine.selected_[1] &&
+         val_.second == engine.selected_[2]) ||
+        (val_.second == engine.selected_[0] &&
+         val_.first == engine.selected_[1]) |
+        (val_.second == engine.selected_[1] &&
+         val_.first == engine.selected_[2])) {
+        SDL_SetRenderDrawColor(engine.ren_, 0xff, 0x00, 0x00, 0xff);
+    } else {
+        SDL_SetRenderDrawColor(engine.ren_, 0xff, 0xff, 0xff, 0xff);
+    }
+
     SDL_RenderDrawLine(engine.ren_, a.first, a.second,
             b.first, b.second);
 }
