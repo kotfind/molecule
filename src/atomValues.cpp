6f9f1ad84adbe7636ed149a8deb985fadd29d480
kotfind <kotfind@yandex.ru>
2021-10-10 14:17:52 +0300

    Add O into atomValues

--- src/atomValues.cpp
+++ src/atomValues.cpp
@@ -7,6 +7,7 @@ std::unordered_map<std::string, std::tuple<char, char, char>> atomColours = {
     {"H", {0xFF, 0xFF, 0xFF}},
     {"F", {0x00, 0xFF, 0xFF}},
     {"N", {0x00, 0x00, 0xFF}},
+    {"O", {0xFF, 0x00, 0x00}},
 };
 
 std::unordered_map<std::string, double> atomRadii = {
@@ -14,4 +15,5 @@ std::unordered_map<std::string, double> atomRadii = {
     {"H", .32},
     {"F", .65},
     {"N", .75},
+    {"O", .73},
 };
